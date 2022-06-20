6b84335791a00c88c6cb6f8c1c84750f1a375fb3
sanlokaja <sandyloka790@gmail.com>
2022-06-20 20:10:16 +0700

    materials/change-attribute

--- migrations/20220614142355-create-products.js
+++ migrations/20220614142355-create-products.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      product_id: {
+      user_id: {
         type: Sequelize.INTEGER
       },
       name: {
